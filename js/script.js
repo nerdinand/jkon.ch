31be949f66a156eefc4daeef68be76d8df37e090
Florian Amoser <florian@amoser.ch>
2018-12-13 22:09:09 +0100

    footer link target corrections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     // document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
-    this.console.log(this.pageYOffset);
+    // this.console.log(this.pageYOffset);
     if (this.pageYOffset > 50) {
         document.getElementById("jkon-logo").style.display = "none";
   } else {
@@ -8,3 +8,9 @@ window.addEventListener('scroll', function() {
   }
 });
 
+function initalize(){
+document.getElementsByClassName('username')[0].innerText = 'facebook: jungekunstolten'
+document.getElementsByClassName('username')[1].innerText = 'instagram: jkonolten'
+document.getElementsByClassName('social-media-list')[0].children[0].childNodes[0].target = "_blank"
+document.getElementsByClassName('social-media-list')[0].children[1].childNodes[0].target = "_blank"
+};
