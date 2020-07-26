99082498b101b221c5eae9f1ffbd7d0d334a5756
nerdinand <nerdinand@nerdinand.com>
2020-07-26 10:10:26 +0200

    Towards a better artists page

--- scripts/image-resize-script/resize.rb
+++ scripts/image-resize-script/resize.rb
@@ -4,6 +4,7 @@ SMALL_DIR = '../../images/artists/2020/small'
 
 Dir["#{SOURCE_DIR}/*"].each do |original_file|
   filename = File.basename(original_file)
-  `convert #{original_file} -resize 740x\\> #{BIG_DIR}/#{filename}`
-  `convert #{original_file} -resize 240x\\> #{SMALL_DIR}/#{filename}`
+  # `convert #{original_file} -resize 740x\\> #{BIG_DIR}/#{filename}`
+  # `convert #{original_file} -resize 240x\\> #{SMALL_DIR}/#{filename}`
+  `convert #{original_file} -resize "240x320^" -gravity center -crop 240x320+0+0 +repage #{SMALL_DIR}/#{filename}`
 end
