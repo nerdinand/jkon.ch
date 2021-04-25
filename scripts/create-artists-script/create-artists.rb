a6298a7aa2906bd204ddec3ff03e1d652523f788
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-04-25 19:48:14 +0200

    Announce 2021 artists

---
+++ scripts/create-artists-script/create-artists.rb
@@ -0,0 +1,27 @@
+artist_list = "Aathmigan Jegatheeswaran
+Andrea Francesco Todisco
+Emanuel Heim
+Cheyenne Oswald
+Stéphanie Kiser
+Victoria Holdt
+Lorenz Fischer
+Chevalley Line
+Mina Achermann
+Irene Gazillo
+Genevieve Leong
+Alexandra Feusi
+Zhaoyue Fan
+Nathalie Stirnimann & Stefan Stojanovic"
+
+artist_names = artist_list.split("\n")
+
+template_source = File.read('scripts/create-artists-script/template.html')
+
+year = 2021
+
+artist_names.each do |artist_name|
+  File.write(
+    "_artists/#{year}/#{artist_name}.html",
+    template_source.gsub('__NAME__', artist_name).gsub('__YEAR__', year.to_s)
+  )
+end
