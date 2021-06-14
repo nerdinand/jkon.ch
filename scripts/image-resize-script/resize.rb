ca6ce07ee9b80528240f73941e18098cdd1e4670
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-06-14 23:28:25 +0200

    Add image gallery for JKON 2020, clean up

--- scripts/image-resize-script/resize.rb
+++ scripts/image-resize-script/resize.rb
@@ -1,9 +1,14 @@
-SOURCE_DIR = '../../images/artists/2019/original'
-BIG_DIR = '../../images/artists/2019/big'
-SMALL_DIR = '../../images/artists/2019/small'
+SOURCE_DIR = '../../images/past-exhibitions/2020/original'
+BIG_DIR = '../../images/past-exhibitions/2020/big'
+SMALL_DIR = '../../images/past-exhibitions/2020/small'
+
+BIG_MAX_WIDTH = 740
+SMALL_MAX_WIDTH = 240
+SMALL_MAX_HEIGHT = 320
 
 Dir["#{SOURCE_DIR}/*"].each do |original_file|
   filename = File.basename(original_file)
-  `convert #{original_file} -resize 740x\\> #{BIG_DIR}/#{filename}`
-  `convert #{original_file} -resize "240x320^" -gravity center -crop 240x320+0+0 +repage #{SMALL_DIR}/#{filename}`
+  puts filename
+  `convert #{original_file} -resize #{BIG_MAX_WIDTH}x\\> #{BIG_DIR}/#{filename}`
+  `convert #{original_file} -resize "#{SMALL_MAX_WIDTH}x#{SMALL_MAX_HEIGHT}^" -gravity center -crop #{SMALL_MAX_WIDTH}x#{SMALL_MAX_HEIGHT}+0+0 +repage #{SMALL_DIR}/#{filename}`
 end
