0dd1983d9ba65d6ddc69750ca93a8c8052d9ae0e
Ferdinand Niedermann <ferdinand.niedermann@gmail.com>
2021-06-15 00:09:18 +0200

    More past-exhibitions related cleanup

--- scripts/image-resize-script/resize.rb
+++ scripts/image-resize-script/resize.rb
@@ -3,12 +3,12 @@ BIG_DIR = '../../images/past-exhibitions/2020/big'
 SMALL_DIR = '../../images/past-exhibitions/2020/small'
 
 BIG_MAX_WIDTH = 740
-SMALL_MAX_WIDTH = 240
-SMALL_MAX_HEIGHT = 320
+SMALL_WIDTH = 240
+SMALL_HEIGHT = 320
 
 Dir["#{SOURCE_DIR}/*"].each do |original_file|
   filename = File.basename(original_file)
   puts filename
   `convert #{original_file} -resize #{BIG_MAX_WIDTH}x\\> #{BIG_DIR}/#{filename}`
-  `convert #{original_file} -resize "#{SMALL_MAX_WIDTH}x#{SMALL_MAX_HEIGHT}^" -gravity center -crop #{SMALL_MAX_WIDTH}x#{SMALL_MAX_HEIGHT}+0+0 +repage #{SMALL_DIR}/#{filename}`
+  `convert #{original_file} -resize "#{SMALL_WIDTH}x#{SMALL_HEIGHT}^" -gravity center -crop #{SMALL_WIDTH}x#{SMALL_HEIGHT}+0+0 +repage #{SMALL_DIR}/#{filename}`
 end
