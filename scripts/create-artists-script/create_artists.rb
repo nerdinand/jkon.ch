5559dc25a323410d17126c19fd4a3e6a36ecd09c
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2022-04-27 20:16:13 +0200

    Add 2022 artists list

--- scripts/create-artists-script/create_artists.rb
+++ scripts/create-artists-script/create_artists.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
-artist_list = "Aathmigan Jegatheeswaran
-Andrea Francesco Todisco
-Emanuel Heim
-Cheyenne Oswald
-Stéphanie Kiser
-Victoria Holdt
-Lorenz Fischer
-Chevalley Line
-Mina Achermann
-Irene Gazillo
-Genevieve Leong
-Alexandra Feusi
-Zhaoyue Fan
-Nathalie Stirnimann & Stefan Stojanovic"
+artist_list = "Lars Tuchel
+Noah Ismael Wyss
+Manuela Cossalter
+Isabelle Benvenuti
+Tirza Matter
+Emile Van Helleputte
+Anita Moser
+Benjamin Schwander
+Teddy Pratt
+Marc Lauber
+Catherin Schöberl"
 
 artist_names = artist_list.split("\n")
 
 template_source = File.read('scripts/create-artists-script/template.html')
 
-year = 2021
+year = 2022
 
 artist_names.each do |artist_name|
   File.write(
