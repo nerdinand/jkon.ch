41ed659d1ace811ab8a87c6b3cd4ed277de5d907
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2022-07-03 19:48:02 +0200

    Add 2022 artists

--- _includes/index/flyer.fr.md
+++ _includes/index/flyer.fr.md
@@ -1,7 +1,4 @@
 <hr>
-<a href="images/flyers/JKON2021_Flyer_FR-1.png">
-	<img src="images/flyers/JKON2021_Flyer_FR-1.png" width="350" />
-</a>
-<a href="images/flyers/JKON2021_Flyer_FR-2.png">
-	<img src="images/flyers/JKON2021_Flyer_FR-2.png" width="350" />
+<a href="images/flyers/JKON2022_Flyer.jpg">
+	<img src="images/flyers/JKON2022_Flyer.jpg" width="700" />
 </a>
--- scripts/image-resize-script/resize.rb
+++ scripts/image-resize-script/resize.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 
-SOURCE_DIR = '../../images/artists/2021/original'
-BIG_DIR = '../../images/artists/2021/big'
-SMALL_DIR = '../../images/artists/2021/small'
+if ARGV.size != 1
+  puts "Usage: ruby resize.rb <root_dir>"
+  puts "  e.g. `ruby resize.rb ../../images/artists/2022/`"
+  puts "  Script assumes that <root_dir> contains the subdirectories `original`, `big`, `small`."
+  puts "  Will generate resized images for each image in `original` and store them in `big` and `small`."
+  exit(1)
+end
+
+root_dir = ARGV[0]
+
+ORIGINAL_DIR = "#{root_dir}/original"
+BIG_DIR = "#{root_dir}/big"
+SMALL_DIR = "#{root_dir}/small"
 
 BIG_MAX_WIDTH = 740
 SMALL_WIDTH = 240
 SMALL_HEIGHT = 320
 
-Dir["#{SOURCE_DIR}/*"].each do |original_file|
+Dir["#{ORIGINAL_DIR}/*"].each do |original_file|
   filename = File.basename(original_file)
   puts filename
 
-  `convert #{original_file} \
-    -resize #{BIG_MAX_WIDTH}x\\> #{BIG_DIR}/#{filename}`
+  `convert "#{original_file}" \
+    -resize #{BIG_MAX_WIDTH}x\\> "#{BIG_DIR}/#{filename}"`
 
-  `convert #{original_file} \
+  `convert "#{original_file}" \
     -resize "#{SMALL_WIDTH}x#{SMALL_HEIGHT}^" \
     -gravity center \
     -crop #{SMALL_WIDTH}x#{SMALL_HEIGHT}+0+0 \
-    +repage #{SMALL_DIR}/#{filename}`
+    +repage "#{SMALL_DIR}/#{filename}"`
 end
