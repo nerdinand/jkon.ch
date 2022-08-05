03dd4ad6673276a684a2e4bafe4610158562378c
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2022-08-05 17:53:18 +0200

    Optimise build performance: Don't include original images in site

--- scripts/image-resize-script/resize.rb
+++ scripts/image-resize-script/resize.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
-if ARGV.size != 1
-  puts 'Usage: ruby resize.rb <root_dir>'
-  puts '  e.g. `ruby resize.rb ../../images/artists/2022/`'
-  puts '  Script assumes that <root_dir> contains the subdirectories `original`, `big`, `small`.'
-  puts '  Will generate resized images for each image in `original` and store them in `big` and `small`.'
+if ARGV.size != 2
+  puts 'Usage: ruby resize.rb <source_dir> <destination_dir>'
+  puts '  e.g. `ruby resize.rb ../../images/artists/original/2022/ ../../images/artists/2022/`'
+  puts '  Script assumes that <destination_dir> contains the subdirectories `big`, `small`.'
+  puts '  Will generate resized images for each image in `source_dir` and store them in `destination_dir/big` and `destination_dir/small`.'
   exit(1)
 end
 
-root_dir = ARGV[0]
+source_dir = ARGV[0]
+destination_dir = ARGV[1]
 
-ORIGINAL_DIR = "#{root_dir}/original".freeze
-BIG_DIR = "#{root_dir}/big".freeze
-SMALL_DIR = "#{root_dir}/small".freeze
+ORIGINAL_DIR = "#{source_dir}/original".freeze
+BIG_DIR = "#{destination_dir}/big".freeze
+SMALL_DIR = "#{destination_dir}/small".freeze
 
 BIG_MAX_WIDTH = 740
 SMALL_WIDTH = 240
