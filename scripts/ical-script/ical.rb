b6ffc84af955f01ae3858187684ac6f593c02e31
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2025-05-20 12:43:40 +0200

    Add ics files for vernissage and finissage

--- scripts/ical-script/ical.rb
+++ scripts/ical-script/ical.rb
@@ -3,73 +3,24 @@
 require 'icalendar'
 require 'pry-byebug'
 
-cal = Icalendar::Calendar.new
+@description = 'Junge Kunst Olten 2025'
+@url = 'https://www.jkon.ch'
+@location = 'Kunstverein Olten, Hübelistrasse 30, 4600 Olten'
 
-summary = 'JKON 2022'
-description = 'Junge Kunst Olten 2022'
-url = 'https://www.jkon.ch'
+def create_ical(file_name, dtstart, summary)
+  cal = Icalendar::Calendar.new
 
-location_schuetzi = 'Schützi Olten, Schützenmattweg 7, 4600 Olten'
-
-# Thursday
-cal.event do |event|
-  event.dtstart     = DateTime.civil(2022, 8, 25, 18, 0)
-  event.dtend       = DateTime.civil(2022, 8, 25, 22, 0)
-  event.summary     = summary
-  event.description = description
-  event.location    = location_schuetzi
-  event.url         = url
-end
-
-# Friday
-cal.event do |event|
-  event.dtstart     = DateTime.civil(2022, 8, 26, 18, 0)
-  event.dtend       = DateTime.civil(2022, 8, 26, 22, 0)
-  event.summary     = summary
-  event.description = description
-  event.location    = location_schuetzi
-  event.url         = url
-end
-
-# Saturday
-cal.event do |event|
-  event.dtstart     = DateTime.civil(2022, 8, 27, 16, 0)
-  event.dtend       = DateTime.civil(2022, 8, 27, 19, 0)
-  event.summary     = summary
-  event.description = description
-  event.location    = location_schuetzi
-  event.url         = url
-end
-
-# Opening
-cal.event do |event|
-  event.dtstart     = DateTime.civil(2022, 8, 25, 18, 30)
-  event.summary     = 'JKON 2022 Vernissage'
-  event.description = description
-  event.location    = location_schuetzi
-  event.url         = url
-end
-
-# Closing
-cal.event do |event|
-  event.dtstart     = DateTime.civil(2022, 8, 27, 17, 30)
-  event.summary     = 'JKON 2022 Finissage'
-  event.description = description
-  event.location    = location_schuetzi
-  event.url         = url
-end
-
-# Guided tours
-[DateTime.civil(2022, 8, 26, 19, 0), DateTime.civil(2022, 8, 27, 16, 0)].each do |datetime|
   cal.event do |event|
-    event.dtstart     = datetime
-    event.summary     = 'JKON 2022 Werkgespräche'
-    event.description = description
-    event.location    = location_schuetzi
-    event.url         = url
+    event.dtstart     = dtstart
+    event.summary     = summary
+    event.description = @description
+    event.location    = @location
+    event.url         = @url
   end
+
+  cal.publish
+  File.write(file_name, cal.to_ical)
 end
 
-cal.publish
-
-File.write('../../ical/jkon2022.ics', cal.to_ical)
+create_ical('../../ical/jkon2025-vernissage.ics', DateTime.civil(2025, 8, 30, 17, 15), 'JKON 2025 Vernissage')
+create_ical('../../ical/jkon2025-finissage.ics', DateTime.civil(2025, 9, 27, 16, 15), 'JKON 2025 Finissage')
